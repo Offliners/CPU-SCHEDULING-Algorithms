24ab38190f726a889bbc0185e13a1611e0f7f0d1
Offliner <42777325+Offliners@users.noreply.github.com>
2020-08-13 16:42:45 +0800

    Create get_time.c

---
+++ kernel_files/get_time.c
@@ -0,0 +1,13 @@
+// syscall #334
+
+#include <linux/linkage.h>
+#include <linux/kernel.h>
+#include <linux/timer.h>
+
+asmlinkage long sys_pj1_time(void)
+{
+    static const long BASE = 1000000000;
+    struct timespec t;
+    getnstimeofday(&t);
+    return t.tv_sec * BASE + t.tv_nsec;
+}
